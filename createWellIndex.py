8b484a0c2802e26344bae2ad0fc5bd9c07b07445
wxhts <wxhts@users.noreply.github.com>
2016-11-01 16:00:28 -0400

    Create createWellIndex.py

---
+++ createWellIndex.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+def createWellIndex(file_path):
+
+    df = pd.read_csv(file_path)
+    wells = pd.Series(df['Well'])
+    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    num = '0123456789'
+
+    # Split row and column sequentially and append to lists
+    colnum = []
+    letters = []
+    for i in wells:
+        nums = int(i.strip(alpha))
+        lett = i.strip(num)
+        colnum.append(nums)
+        letters.append(lett)
+
+    # Map row number to alpha index and some logic
+    rownums = []
+    for l in letters:
+        if l == 'AA':
+            n = 27
+        elif l == 'AB':
+            n = 28
+        elif l == 'AC':
+            n = 29
+        elif l == 'AD':
+            n = 30
+        elif l == 'AE':
+            n = 31
+        elif l == 'AF':
+            n = 32
+        elif l in alpha:
+            n = alpha.index(l) + 1
+        rownums.append(n)
+
+    # Add lists as new columns to dataframe
+    reversedRows = [(x * -1) for x in rownums]
+    df['Column'] = colnum
+    df['Row'] = rownums
+    df['Reverse Row'] = reversedRows
+
+    return df
